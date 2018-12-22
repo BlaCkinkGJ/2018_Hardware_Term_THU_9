023acd8995258fe3543cfeb6b0f7d3181cc4af49
BlaCkinkGJ <ss5kijun@gmail.com>
2018-12-22 19:53:44 +0900

    하드웨어의 안정성을 증대시키도록 함
    스캔을 좌,우로 가능하게 하였고, 부저가 울리도록 만들었으며 스캔 시 발생하는 오차를 해결하였습니다.

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 #include "ultra.h"
 #include "servo.h"
 
+#define FORWARD 1
+#define BACKWARD -1
+
 HRS04_VAR ultra1;
 // HRS04_VAR ultra2;
 // HRS04_VAR ultra3;
@@ -13,56 +16,57 @@ PWM pwm;
 
 /// For ultra1
 void TIM3_IRQHandler(void){
-	if( TIM_GetITStatus(ultra1.timer, ultra1.timer_cc_channel)==SET ){
-		TIM_ClearITPendingBit(ultra1.timer, ultra1.timer_cc_channel);
-		if(GPIO_ReadInputDataBit(ultra1.timer_port, ultra1.timer_pin)==Bit_SET){ // if timer is high
-			ultra1.cap_rising_edge = TIM_GetCapture1(ultra1.timer);	// read capture data
-			(ultra1.timer)->CCER |=  ultra1.timer_ccip;	// to falling edge
-		}
-		else{ // if timer is low
-			ultra1.cap_falling_edge = TIM_GetCapture1(ultra1.timer);	// read capture data
-			ultra1.pulse_width = (u32) (ultra1.cap_falling_edge - ultra1.cap_rising_edge);
-			(ultra1.timer)->CCER &= ~ ultra1.timer_ccip;	// to rising edge
-			ultra1.distance = true;
-		} // end if 
-	} //  end if
+    if( TIM_GetITStatus(ultra1.timer, ultra1.timer_cc_channel)==SET ){
+        TIM_ClearITPendingBit(ultra1.timer, ultra1.timer_cc_channel);
+        if(GPIO_ReadInputDataBit(ultra1.timer_port, ultra1.timer_pin)==Bit_SET){ // if timer is high
+            ultra1.cap_rising_edge = TIM_GetCapture1(ultra1.timer);    // read capture data
+            (ultra1.timer)->CCER |=  ultra1.timer_ccip;    // to falling edge
+        }
+        else{ // if timer is low
+            ultra1.cap_falling_edge = TIM_GetCapture1(ultra1.timer);    // read capture data
+            ultra1.pulse_width = (u32) (ultra1.cap_falling_edge - ultra1.cap_rising_edge);
+            (ultra1.timer)->CCER &= ~ ultra1.timer_ccip;    // to rising edge
+            ultra1.distance = true;
+        } // end if 
+    } //  end if
 }
 
 static void ultra_init(){
-	init_hrsd04_variable(&ultra1);
-	ultra1.trig_port         = GPIOA;
-	ultra1.trig_pin          = GPIO_Pin_7;
-	ultra1.timer             = TIM3;
-	ultra1.timer_port        = GPIOA;
-	ultra1.timer_pin         = GPIO_Pin_6;
-	ultra1.timer_inp_channel = TIM_Channel_1;
-	ultra1.timer_cc_channel  = TIM_IT_CC1;
-	ultra1.timer_ccip        = TIM_CCER_CC1P;
-	ultra1.timer_rcc         = RCC_APB1Periph_TIM3;
-	ultra1.timer_irq         = TIM3_IRQn;
-	ultra1.trig_rcc          = RCC_APB2Periph_GPIOA;
+    init_hrsd04_variable(&ultra1);
+    ultra1.trig_port         = GPIOA;
+    ultra1.trig_pin          = GPIO_Pin_7;
+    ultra1.timer             = TIM3;
+    ultra1.timer_port        = GPIOA;
+    ultra1.timer_pin         = GPIO_Pin_6;
+    ultra1.timer_inp_channel = TIM_Channel_1;
+    ultra1.timer_cc_channel  = TIM_IT_CC1;
+    ultra1.timer_ccip        = TIM_CCER_CC1P;
+    ultra1.timer_rcc         = RCC_APB1Periph_TIM3;
+    ultra1.timer_irq         = TIM3_IRQn;
+    ultra1.trig_rcc          = RCC_APB2Periph_GPIOA;
 }
 
 static void pwm_setting(){
-	pwm.OCMode    = TIM_OCMode_PWM1;
-	pwm.rcc_timer = RCC_APB1Periph_TIM4;
-	pwm.timer     = TIM4;
-	pwm.rcc_gpio  = RCC_APB2Periph_GPIOB;
-	pwm.gpio_port = GPIOB;
-	pwm.gpio_pin  = GPIO_Pin_8;
-	pwm_init(&pwm);
+    pwm.OCMode    = TIM_OCMode_PWM1;
+    pwm.rcc_timer = RCC_APB1Periph_TIM4;
+    pwm.timer     = TIM4;
+    pwm.rcc_gpio  = RCC_APB2Periph_GPIOB;
+    pwm.gpio_port = GPIOB;
+    pwm.gpio_pin  = GPIO_Pin_8;
+    pwm_init(&pwm);
 }
 
 int counter = 0;
 int now = 0;
 bool isCheck = false;
+int direction = FORWARD;
 
 static void setup(void){
-	SystemInit();
+    SystemInit();
     ultra_init();
-	pwm_setting();
-	pwm_init(&pwm);
-	ultra_setup(&ultra1);
+    pwm_setting();
+    pwm_init(&pwm);
+    ultra_setup(&ultra1);
 
     ultra1.capture = true;
     Triger_InputSig(&ultra1);
@@ -70,59 +74,69 @@ static void setup(void){
 
 static void loop(void)
 {
-	int isPeople = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1);
+    int isPeople = GPIO_ReadInputDataBit(GPIOC, GPIO_Pin_1);
 
-	if(isPeople == 0){
-		GPIO_SetBits(GPIOD, GPIO_Pin_4);
-	}else{
-		GPIO_ResetBits(GPIOD, GPIO_Pin_4);
-	}
+    if(isPeople == 0){
+        GPIO_SetBits(GPIOD, GPIO_Pin_4);
+    }else{
+        GPIO_ResetBits(GPIOD, GPIO_Pin_4);
+    }
 
-	GPIO_SetBits(GPIOD, GPIO_Pin_3);
-	change_pwm_cycle(&pwm, 180);
-	if(ultra1.distance != false){ // distance analysis
-		int dist_mm = (ultra1.pulse_width*17/100);
-		ultra1.distance = false;
-		if(ultra1.capture){ // if capture is enabled
-			if(dist_mm < 1000){
-				counter++;
-				if(counter > 3){
-					GPIO_ResetBits(GPIOD, GPIO_Pin_2);
-					change_pwm_cycle(&pwm, now);
-					isCheck = true;
-				}
-			}
-			else{
-				GPIO_SetBits(GPIOD, GPIO_Pin_2);
-				counter = counter <= 0 ? 0 : counter - 1;
-				isCheck = false;
-			}
-			Triger_InputSig(&ultra1);
+    GPIO_SetBits(GPIOD, GPIO_Pin_3);
+    change_pwm_cycle(&pwm, 180);
+    if(ultra1.distance != false){ // distance analysis
+        int dist_mm = (ultra1.pulse_width*17/100);
+        ultra1.distance = false;
+        if(ultra1.capture){ // if capture is enabled
+            if(dist_mm < 1050){
+                counter = counter + 1 > 5 ? 5 : counter + 1;
+                if(counter > 3){
+                    GPIO_ResetBits(GPIOD, GPIO_Pin_2);
+                    GPIO_SetBits(GPIOD, GPIO_Pin_7);
+                    change_pwm_cycle(&pwm, now);
+                    isCheck = true;
+                }
+            }
+            else{
+                GPIO_SetBits(GPIOD, GPIO_Pin_2);
+                GPIO_ResetBits(GPIOD, GPIO_Pin_7);
+                counter = 0;
+                isCheck = false;
+            }
 
-		} // end of if
-	} // end of if
-	if(!isCheck){
-		now = (now + 10) % 180;
-		change_pwm_cycle(&pwm, now);
-	}
-	GPIO_ResetBits(GPIOD, GPIO_Pin_3);
-	delay_ms(500);
+        } // end of if
+    } // end of if
+    if(!isCheck){
+        now = now + 20*direction;
+        if(now > 180){
+            direction = BACKWARD;
+            now = 180;
+        }
+        if(now < 0){
+            direction = FORWARD;
+            now = 0;
+        }
+        change_pwm_cycle(&pwm, now);
+    }
+    Triger_InputSig(&ultra1);
+    GPIO_ResetBits(GPIOD, GPIO_Pin_3);
+    delay_ms(250);
 }
 
 
 int main(void){
-	GPIO_InitTypeDef gpio_init_struct;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
-	gpio_init_struct.GPIO_Mode = GPIO_Mode_Out_PP;
-	gpio_init_struct.GPIO_Pin = (GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4);
-	gpio_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOD, &gpio_init_struct);
+    GPIO_InitTypeDef gpio_init_struct;
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+    gpio_init_struct.GPIO_Mode = GPIO_Mode_Out_PP;
+    gpio_init_struct.GPIO_Pin = (GPIO_Pin_2|GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_7);
+    gpio_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &gpio_init_struct);
 
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-	gpio_init_struct.GPIO_Mode = GPIO_Mode_IPD;
-	gpio_init_struct.GPIO_Pin = (GPIO_Pin_1);
-	gpio_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOD, &gpio_init_struct);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    gpio_init_struct.GPIO_Mode = GPIO_Mode_IPD;
+    gpio_init_struct.GPIO_Pin = (GPIO_Pin_1);
+    gpio_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &gpio_init_struct);
 
     setup();
     while(true){
